47107e9b2ae20f027d1051635ef4cc6e9512d515
Ablos <dieterich.abel@gmail.com>
2020-02-13 00:10:43 +0100

    Moved some code

--- ServerManager/Functions.cs
+++ ServerManager/Functions.cs
@@ -3,6 +3,9 @@ using System;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Linq;
+using System.Drawing;
 
 namespace ServerManager
 {
@@ -123,6 +126,56 @@ namespace ServerManager
                 pb.Value = value;               // Move to correct value
             }
             catch { }
+        }
+    }
+
+    public class ProcessCounter
+    {
+        public static PerformanceCounter GetPerfCounterForProcessId(int processId, string processCounterName = "% Processor Time")
+        {
+            string instance = GetInstanceNameForProcessId(processId);
+            if (string.IsNullOrEmpty(instance))
+                return null;
+
+            return new PerformanceCounter("Process", processCounterName, instance);
+        }
+
+        public static string GetInstanceNameForProcessId(int processId)
+        {
+            var process = Process.GetProcessById(processId);
+            string processName = Path.GetFileNameWithoutExtension(process.ProcessName);
+
+            PerformanceCounterCategory cat = new PerformanceCounterCategory("Process");
+            string[] instances = cat.GetInstanceNames()
+                .Where(inst => inst.StartsWith(processName))
+                .ToArray();
+
+            foreach (string instance in instances)
+            {
+                using (PerformanceCounter cnt = new PerformanceCounter("Process",
+                    "ID Process", instance, true))
+                {
+                    int val = (int)cnt.RawValue;
+                    if (val == processId)
+                    {
+                        return instance;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+
+    public static class RichTextBoxExtensions
+    {
+        public static void AppendText(this RichTextBox box, string text, Color color)
+        {
+            box.SelectionStart = box.TextLength;
+            box.SelectionLength = 0;
+
+            box.SelectionColor = color;
+            box.AppendText(text);
+            box.SelectionColor = box.ForeColor;
         }
     }
 }
